39d96193e5e22a8158b2f7aaf76157e1c3a4d6c2
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2023-01-10 14:50:53 +0900

    create a contract to distribute sbt

---
+++ hashlips_nft_contract-main/info/kangeki.sol
@@ -0,0 +1,166 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2023 Ryo Miyata
+
+/*
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
+pragma solidity >=0.7.0 <0.9.0;
+
+import 'erc721a/contracts/ERC721A.sol';
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract kangeki_contract is ERC721A, Ownable {
+
+    string baseURI;
+    string public baseExtension = ".json";
+    uint256 public cost = 0.05 ether;
+    uint256 public maxSupply = 40;
+    uint256 public preSaleSupply = 10;
+    uint256 public maxMintAmount = 3;
+    bool public paused = true;
+    bool public onlyAllowListed = true;
+    mapping(address => uint256) public allowListUserAmount;
+    mapping(address => uint256) public allowListMintedAmount;
+    
+
+    constructor(
+    ) ERC721A('Kangeki', 'KG') {
+        setBaseURI('ipfs://QmWC1Ai5JBf1CbvHF3SXVp1vxecPdfF7RkKz1oG4rVrR3u/');
+    }
+
+    // internal
+    function _baseURI() internal view virtual override returns (string memory) {
+        return baseURI;
+        
+    }
+
+    // public
+    function mint(uint256 _mintAmount) public payable {
+        require(!paused, "the contract is paused");
+        uint256 supply = totalSupply();
+        require(_mintAmount > 0, "need to mint at least 1 NFT");
+        require(_mintAmount <= maxMintAmount, "max mint amount per session exceeded");
+        require(supply + _mintAmount <= maxSupply, "max NFT limit exceeded");
+        require(supply + _mintAmount <= preSaleSupply, "pre Sale NFT limit exceeded");
+
+        // Owner also can mint.
+        if (msg.sender != owner()) {
+            require(msg.value >= cost * _mintAmount, "insufficient funds");
+            if(onlyAllowListed == true) {
+                require(allowListUserAmount[msg.sender] != 0, "user is not allowlisted");
+                require(allowListMintedAmount[msg.sender] + _mintAmount <= allowListUserAmount[msg.sender], "max NFT per address exceeded");
+                allowListMintedAmount[msg.sender] += _mintAmount;
+            }
+        }
+
+        _safeMint(msg.sender, _mintAmount);
+    }
+
+    function airdropMint(address[] calldata _airdropAddresses , uint256[] memory _UserMintAmount) public onlyOwner{
+        uint256 supply = totalSupply();
+        uint256 _mintAmount = 0;
+        for (uint256 i = 0; i < _UserMintAmount.length; i++) {
+            _mintAmount += _UserMintAmount[i];
+        }
+        require(_mintAmount > 0, "need to mint at least 1 NFT");
+        require(supply + _mintAmount <= maxSupply, "max NFT limit exceeded");
+
+        for (uint256 i = 0; i < _UserMintAmount.length; i++) {
+            _safeMint(_airdropAddresses[i], _UserMintAmount[i] );
+        }
+    }
+
+    function setAllowList(address[] memory addresses, uint256[] memory saleSupplies) public onlyOwner {
+        require(addresses.length == saleSupplies.length);
+        for (uint256 i = 0; i < addresses.length; i++) {
+            allowListUserAmount[addresses[i]] = saleSupplies[i];
+        }
+    }
+
+
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory){
+        return string(abi.encodePacked(ERC721A.tokenURI(tokenId), baseExtension));
+    }
+
+    //only owner  
+    function setCost(uint256 _newCost) public onlyOwner {
+        cost = _newCost;
+    }
+
+    function setOnlyAllowListed(bool _state) public onlyOwner {
+        onlyAllowListed = _state;
+    }    
+
+    function setpreSaleSupply(uint256 _newpreSaleSupply) public onlyOwner {
+        preSaleSupply = _newpreSaleSupply;
+    }
+
+    function setmaxMintAmount(uint256 _newmaxMintAmount) public onlyOwner {
+        maxMintAmount = _newmaxMintAmount;
+    }
+  
+    function setBaseURI(string memory _newBaseURI) public onlyOwner {
+        baseURI = _newBaseURI;
+    }
+
+    function setBaseExtension(string memory _newBaseExtension) public onlyOwner {
+        baseExtension = _newBaseExtension;
+    }
+
+    function pause(bool _state) public onlyOwner {
+        paused = _state;
+    }
+ 
+    function withdraw() public payable onlyOwner {
+        (bool os, ) = payable(owner()).call{value: address(this).balance}("");
+        require(os);
+    }
+
+    function _startTokenId() internal view virtual override returns (uint256) {
+        return 1;
+    }    
+
+    //
+    //sbt section
+    //
+
+    bool public isSBT = false;
+
+    function setIsSBT(bool _state) public onlyOwner {
+        isSBT = _state;
+    }
+
+    function _beforeTokenTransfers( address from, address to, uint256 startTokenId, uint256 quantity) internal virtual override{
+        require( isSBT == false || from == address(0) || to == address(0x000000000000000000000000000000000000dEaD), "transfer is prohibited");
+        super._beforeTokenTransfers(from, to, startTokenId, quantity);
+    }
+
+    function setApprovalForAll(address operator, bool approved) public virtual override {
+        require( isSBT == false , "setApprovalForAll is prohibited");
+        super.setApprovalForAll(operator, approved);
+    }
+
+    function approve(address to, uint256 tokenId) public payable virtual override {
+        require( isSBT == false , "approve is prohibited");
+        super.approve(to, tokenId);
+    }
+}
